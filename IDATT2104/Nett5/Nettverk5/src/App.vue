acd15554ab9249a986dc719e4a9e330821df10c2
Sverre <sverre.frogner.haugen@gmail.com>
2024-09-23 21:13:55 +0200

    Added last 2 network practices

---
+++ IDATT2104/Nett5/Nettverk5/src/App.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header id = "LogoArea">
+    <img alt="Rosegard Logo" class="logo" src="@/assets/Rose.png" width="150" height="150" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Rosegard Code Compiler" />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+#LogoArea{
+  background-color: antiquewhite;
+  border: 3px solid #000;
+  border-right: 1.5px;
+  padding: 5px;
+  padding-left: 15px;
+}
+
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-inactive-link);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ IDATT2104/Nett5/Nettverk5/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Welcome to the simple Rosegard Java code compiler. Enter your code in the field in the
+      top right, then press compile to run the code.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.0rem;
+  position: relative;
+  top: -10px;
+  color: var(--color-heading);
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+
+}
+</style>
